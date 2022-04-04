895e7202ae34bf4016e8d8c58d7d3b064122c4af
kmieciu2003 <kmieciu2003@wp.pl>
2022-04-04 13:26:36 +0000

    2

--- monitor_prd.ps1
+++ monitor_prd.ps1
@@ -1,19 +1,54 @@
+while ($true){
 cls
 Set-AzContext -Subscription 'da6b5497-ec65-44a3-a2d5-1457870f7073'
-$head = @"
-Stan na: "+(getdate).ToString('yyyy-MMdd HH:mm:ss.ms')+"
-
+$start = @"
 <style>
+/* Split the screen in half */
+.split {
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+/* Control the left side */
+.left {
+  left: 0;
+  background-color: gray;
+}
+
+/* Control the right side */
+.right {
+  right: 0;
+  background-color: white;
+}
+
+/* If you want the content centered horizontally and vertically */
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
     body
   {
-      background-color: Gainsboro;
+      background-color: white;
   }
 
-    table, th, td{
-      border: 1px solid;
-    }
+  th {
+    background-color: #04AA6D;
+    color: white;
+  }
+  tr:nth-child(even) {background-color: #f2f2f2;}
 </style>
+<h2>Produkcyjne kolejki *_ERROR</h2>
+
 "@
+
 cls 
 $rgName='otd-weu-p-sbn-rg'
 $nsName='otd-weu-p-sbn'
@@ -25,7 +60,9 @@ foreach($queue in (Get-AzServiceBusQueue -ResourceGroupName $rgName -NamespaceNa
    }
    $row=$null
 }
-$tabelka | ConvertTo-Html  -Title "error monitor" -PreContent $head |Out-File .\index.html
+$tabelka | ConvertTo-Html  -Title (get-date).addhours(2).ToString('yyyy-MM-dd HH:mm:ss.ms') -PreContent $start | Out-File .\index.html
 git add .\index.html
 git commit -m (get-date)
 git push
+start-sleep -Seconds 300
+}
